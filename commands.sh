3f8c93b7c28d5e1afc924aff8cf47b8cc8a6dfe5
seynx <43884398+seynx@users.noreply.github.com>
2022-09-14 23:35:19 -0400

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,15 @@
+
+" " = string/double string
+' '= single string
+= equal
+[] = list/turple
+{} = map, curly braces or curly bracket
+() = parentheses
+| = grep, pipe, or
+&& = ampecent / and
+# = hash shbang
+! = alter a condition
+Boolean = true or false
+~ = tilt
+> = greater
+< = less
